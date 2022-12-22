504abe5c3c6bb9860767a79345e688d1e3d25f37
rajatsharmaaa10 <121228298+rajatsharmaaa10@users.noreply.github.com>
2022-12-22 21:31:53 +0530

    Create Assignment 3 Q3.py

---
+++ Assignment 3 Q3.py
@@ -0,0 +1,14 @@
+# Assignment 3 
+# Question 3
+
+print ("tell us how many numbers u wanna give input. ")
+n=input ()
+arr=[]
+for i in range (1,int(n)+1): 
+   set_=()
+   print (f"enter {i}th input " )
+   x=input()
+   set_=(int(x),int(x)*int(x))
+   arr. append (set_)
+
+print (arr)
